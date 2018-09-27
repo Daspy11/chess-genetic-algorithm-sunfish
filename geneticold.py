1c2d8f49cc8a8008584c3ccafa5fd9d2638614bc
Stuart Johnson <stuart@thestu.art>
2018-09-27 14:44:31 +0100

    Update geneticold.py

--- geneticold.py
+++ geneticold.py
@@ -32,18 +32,18 @@ startingPositions = [ #15 hand-selected openings that are frequently played at a
     "rnbqkb1r/pppp1ppp/5n2/4p3/2B1P3/8/PPPP1PPP/RNBQK1NR w KQkq - 2 3" #Berlin Defence 14
 ]
 
-class GeneticAlgorithm():
+class GeneticAlgorithm(): #Use composition?
 
     ###################################### NOT WRITTEN BY ME ######################################
-    def padSolution(self,solution):
-        for k, table in solution.items():
+    def padSolution(self,solution): #Sunfish puts a ring of zeros on the outside of the board to detect whether a given move is valid.
+        for k, table in solution.items(): #This function also pads the piece table so that it matches up.
             padrow = lambda row: (0,) + tuple(x+self.piece[k] for x in row) + (0,)
             solution[k] = sum((padrow(table[i*8:i*8+8]) for i in range(8)), ())
             solution[k] = (0,)*20 + solution[k] + (0,)*20
         return solution
 
     ###################################### WRITTEN BY ME ######################################
-    def __init__(self):
+    def __init__(self): #Constructor that is run when the class is initialized
         self.generationcount = 1
         self.gamesplayed = 1
         self.totalgames = 1
@@ -54,9 +54,11 @@ class GeneticAlgorithm():
         self.piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
 
 
-    def selfPlay(self,pstWhite,pstBlack):
+    def selfPlay(self,pstWhite,pstBlack): 
+        #This function takes in two piece tables and plays a match between them. It returns "W", "D", or "B"
+        #for a white win, draw, or black win. The range of the loop bounds the number of moves played.
         if pstWhite is None or pstBlack is None: print("Null PST in Selfplay")
-        pos = tools.parseFEN(startingPositions[random.randint(0,14)],pstWhite) #Select a random starting opening
+        pos = tools.parseFEN(startingPositions[random.randint(0,14)],pstWhite) #Select a random starting opening from the list
         secs = random.randint(100,300) / 1000 #Random computation time from 10 to 100ms (0.01 to 0.1 seconds). This is to minimize total game repetition. 
         game = []
         for d in range(0,200):
@@ -66,11 +68,11 @@ class GeneticAlgorithm():
             pstCurrent = pstWhite if d % 2 == 0 else pstBlack
             m, _ = sunfish.Searcher().search(pos, secs, pstCurrent) #Modified Sunfish searcher that takes in variable piece tables
             if m is None: #If there are no valid moves
-                return "W" if d % 2 == 0 else "B"
+                return "W" if d % 2 == 0 else "B" #If there are no valid moves that means one side has been checkmated. The side that can't move loses
             #print("\nmove", tools.mrender(pos, m))
             pos = pos.move(m, pstCurrent) #Modify the board state with the selected move.
             if game.count(pos) >= 3: #Check for three move repetition
-                return "D"
+                return "D" #Match is a draw if the current position has appeared at least twice before
             game.append(pos)
         
         return "D" #Declare the game a draw if it doesn't end in 200 moves.
@@ -86,10 +88,10 @@ class GeneticAlgorithm():
             'K': tuple(nums[5])
         }
 
-        return self.padSolution(solution)
+        return self.padSolution(solution) #We need to pad the solution
 
 
-    def generateRandomGeneration(self,size=100):
+    def generateRandomGeneration(self,size=100): #We use a list comprehension to generate a list of random solutions
         return [self.generateRandomSolution() for i in range(size)]
 
     def breedSolution(self,solution1,solution2):
@@ -97,13 +99,13 @@ class GeneticAlgorithm():
         newsolution1 = {
             'P': solution1[p][0:crossover] + solution2[p][crossover:64], #We use python list slicing to get elements before the crossover in solution 1, and after in solution 2
             'N': solution1[n][0:crossover] + solution2[n][crossover:64], #This can be done non-deterministically, however it is not economical for small dicts.
-            'B': solution1[b][0:crossover] + solution2[b][crossover:64],
-            'R': solution1[r][0:crossover] + solution2[r][crossover:64],
+            'B': solution1[b][0:crossover] + solution2[b][crossover:64], #The format [num1:num2] returns the part of the list bounded by
+            'R': solution1[r][0:crossover] + solution2[r][crossover:64], #The two numbers.
             'Q': solution1[q][0:crossover] + solution2[q][crossover:64],
             'K': solution1[k][0:crossover] + solution2[k][crossover:64]
         }
         newsolution2 = {
-            'P': solution2[p][0:crossover] + solution1[p][crossover:64], 
+            'P': solution2[p][0:crossover] + solution1[p][crossover:64], #We create two solutions as there are two parents to replace
             'N': solution2[n][0:crossover] + solution1[n][crossover:64],
             'B': solution2[b][0:crossover] + solution1[b][crossover:64],
             'R': solution2[r][0:crossover] + solution1[r][crossover:64],
@@ -113,11 +115,11 @@ class GeneticAlgorithm():
 
         return [newsolution1,newsolution2]
     
-    def pickRand(self,lst):
+    def pickRand(self,lst): #Pick a random item from a list
         selected = random.randrange(0, len(lst))
         return lst.pop(selected)
 
-    def formPairs(self,lst):
+    def formPairs(self,lst): #Turns a list into a list of lists with length two
         pairs = []
         while lst:
             rand1 = self.pickRand(lst)
@@ -126,7 +128,7 @@ class GeneticAlgorithm():
             pairs.append(pair)
         return pairs
 
-    def run(self):
+    def run(self): #Main loop
         print('Initialising Genetic Algorithm')
         print('Creating population of size 100')
         print('[#',end="")
@@ -138,7 +140,7 @@ class GeneticAlgorithm():
 
         while 1==1:
             pairs = self.formPairs(self.population)
-            for i in range(50):
+            for i in range(50): #50 games for 50 generations. Crossover is not implemented at this point.
                 print('Playing game '+str(self.gamesplayed)+' in generation '+str(self.generationcount))
                 print('W | D | B = '+str(self.whitewins)+' | '+str(self.draws)+' | '+str(self.blackwins))
                 print('[',end="")
